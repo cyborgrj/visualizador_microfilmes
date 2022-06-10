eddae1cf9e6add7c20a2d95765fe92b857e73fe9
Eduardo Rossini <ajuda.apple@gmail.com>
2022-06-10 10:56:28 -0300

    Versão 3.0
    Atualizada com os caminhos corretos de rede para microfilmes, e algoritmo para localização das pastas com os intervalos dos rolos. Falta verificar se a pasta da data e rolo existem e caso contrário retornar com erro para o usuário.

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ while not encerra_programa:
 
     # De acordo com dia mês e ano informados na janela anterior, começa a exibição das imagens encontradas
 
-    caminho_mapas = r'C:\Visualizador de Microfilmes\Mapa Microfilme'
+    caminho_mapas = r'\\192.168.1.153\digitalização\Microfilmes\Microfilmes\Mapa Microfilme'
 
     if programa_fechado !=1:
         PDFMicro = os.path.join(caminho_mapas, ano, nome_pdf)
@@ -67,25 +67,39 @@ while not encerra_programa:
             raise SystemExit()
             break
         if event == 'Ok':
-            arquivo = r"C:\Visualizador de Microfilmes\Mapa Microfilme\2009\030609.PDF"
             print('você escolheu o rolo: {}'.format(values[0]))
             programa_fechado = 0
             break
     microfilme.close()
 
-    rolo_microfilme = values[0]
-    caminho_microfilmes = r'C:\Visualizador de Microfilmes\Microfilmes'
+    rolo_microfilme = int(values[0])
+
+    inicios = [1, 97, 431, 840, 1141, 1397, 1631, 1886, 2138, 2386, 2636, 2889, 3126,
+                3361, 3611, 3862, 4122, 4382, 4537, 4651, 4921, 5191, 5460, 5733, 6014, 6287,
+                6558, 6834, 7101, 7366, 7554, 7751, 7952, 8159, 8366, 8573, 8780, 9004]
+
+    limites = [96, 430, 839, 1140, 1396, 1630, 1885, 2137, 2385, 2635, 2888, 3125, 3360,
+               3610, 3861, 4121, 4381, 4536, 4650, 4920, 5190, 5459, 5732, 6013, 6286, 6557,
+               6833, 7100, 7365, 7553, 7750, 7951, 8158, 8365, 8572, 8779, 9003, 9227]
+
+    for i, lim in enumerate(limites):
+        if rolo_microfilme <= lim:
+            pasta_superior = str(inicios[i]) + ' a ' + str(lim)
+            break
+    print (pasta_superior)
+    caminho_microfilmes = r'\\192.168.1.153\digitalização\Microfilmes\Microfilmes'
 
     thumbArg = r' /filepattern=*.jpg /thumbs'
     # Argumento para abrir a pasta no irfanview no modo de thumbnails (miniaturas)
 
 
     if programa_fechado != 1:
-        pasta_rolo = '"' + os.path.join(caminho_microfilmes, rolo_microfilme) + '"'
+        pasta_rolo = '"' + os.path.join(caminho_microfilmes, pasta_superior, str(rolo_microfilme)) + '"'
+        print(pasta_rolo)
         argumentos = pasta_rolo + thumbArg
         irfanview_path = r'"C:\Program Files\IrfanView\i_view64.exe"'
         cmd = irfanview_path + " " + argumentos
-        #  print(cmd)
+        print(cmd)
         subprocess.call(cmd)
     else:
         raise SystemExit()
---
+++ nomes_pastas.py
@@ -0,0 +1,6 @@
+import os
+
+diretorios = os.listdir(r'\\192.168.1.153\digitalização\Microfilmes\Microfilmes')
+
+for pasta in diretorios:
+    print(pasta)
